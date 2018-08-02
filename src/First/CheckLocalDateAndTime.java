33f96380c9f2e11b4d7798aa5966f169e44b2073
Armin Bajric <bajric.armin.3t1@gmail.com>
2018-08-02 18:26:23 +0200

    Aplikacija koja koristi serversko vrijeme sa http://www.pool.ntp.org te podešava vrijeme na računaru

---
+++ src/First/CheckLocalDateAndTime.java
@@ -0,0 +1,35 @@
+package First;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
+
+public class CheckLocalDateAndTime {
+
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+   
+    URL adresa=new URL("http://www.pool.ntp.org");
+    URLConnection con=adresa.openConnection();
+    long d=con.getDate();
+    
+    Date datum=new Date(d);
+    char[] date=new char[35];
+    String hours=datum.toString();
+    date=hours.toCharArray();
+    String hh=String.valueOf(date[11])+date[12];
+    String mm=String.valueOf(date[14])+date[15];
+    String ss=String.valueOf(date[17])+date[18];
+    String yy=String.valueOf(date[25])+date[26]+date[27]+date[28];
+    String MM="05";
+    String dd=String.valueOf(date[8])+date[9];
+    String strDateToSet=dd+"-"+MM+"-"+yy;
+    String strTimeToSet=hh+":"+mm+":"+ss;
+    
+    Runtime.getRuntime().exec("cmd /C date " + strDateToSet + "& time " + strTimeToSet);
+    System.out.println(hh+mm+ss+yy);
+    
+	}
+
+}

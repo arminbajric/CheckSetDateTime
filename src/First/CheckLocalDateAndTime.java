d4cd4ec0031e6d3c4a9e9295e1f556576f1288b3
Armin Bajric <bajric.armin.3t1@gmail.com>
2018-08-02 18:43:11 +0200

    Aplikacija koja koristi serversko vrijeme europskih date/time servera te podešava računarsko vrijeme.

--- src/First/CheckLocalDateAndTime.java
+++ src/First/CheckLocalDateAndTime.java
@@ -6,7 +6,48 @@ import java.util.Date;
 
 public class CheckLocalDateAndTime {
 
-	
+	public static int getMonth(String month)
+	{
+		int MM = 0;
+		switch(month)
+		{
+		case "Jan":
+			MM=1;
+			break;
+		case "Feb":
+			MM=2;
+			break;
+		case "Mar":
+			MM=3;
+			break;
+		case "Apr":
+			MM=4;
+			break;
+		case "May":
+			MM=5;
+		case "Jun":
+			MM=6;
+			break;
+		case "Jul":
+			MM=7;
+			break;
+		case "Aug":
+			MM=8;
+			break;
+		case "Sep":
+			MM=9;
+			break;
+		case "Oct":
+			MM=10;
+			break;
+		case "Nov":
+			MM=11;
+			break;
+		case "Dec":
+			MM=12;
+		}
+		return MM;
+	}
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
    
@@ -22,14 +63,15 @@ public class CheckLocalDateAndTime {
     String mm=String.valueOf(date[14])+date[15];
     String ss=String.valueOf(date[17])+date[18];
     String yy=String.valueOf(date[25])+date[26]+date[27]+date[28];
-    String MM="05";
+    String MMString=String.valueOf(date[4])+date[5]+date[6];
     String dd=String.valueOf(date[8])+date[9];
+    int MM=getMonth(MMString);
     String strDateToSet=dd+"-"+MM+"-"+yy;
     String strTimeToSet=hh+":"+mm+":"+ss;
     
     Runtime.getRuntime().exec("cmd /C date " + strDateToSet + "& time " + strTimeToSet);
-    System.out.println(hh+mm+ss+yy);
-    
+
+    System.out.println(dd+"-"+MM+"-"+yy);
 	}
 
 }
